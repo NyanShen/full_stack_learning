48a519dedcb31b270e61a61aa29e50d7bf2448bd
NyanShen <sibaoemail@126.com>
2024-04-26 21:28:32 +0800

    [feat]react next learning project

---
+++ nextjs-dashboard/app/dashboard/page.tsx
@@ -0,0 +1,3 @@
+export default function Page() {
+  return <div>dashborad Page in dashborad layout</div>;
+}
---
+++ nyan_next/app/dashboard/page.js
@@ -0,0 +1,11 @@
+export default function Page() {
+    return (
+        <main className="h">
+            <h3 className="w-12 h-32 font-medium">Home</h3>
+            <span className="text-"></span>
+            <p className={`m-0 max-w-[30ch] text-sm opacity-50 text-balance`}>
+                Instantly deploy your Next.js site to a shareable URL with Vercel.
+            </p>
+        </main>
+    )
+}
